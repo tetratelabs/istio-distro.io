d688d13fbe8a07b4868237f85fb28532a5470ac0
Adityo Pratomo <adityo@tetrate.io>
2021-01-27 18:24:13 +0700

    connect form to hubspot
    Signed-off-by: Adityo Pratomo <adityo@tetrate.io>

---
+++ assets/js/form.js
@@ -0,0 +1,61 @@
+function validation() {
+  var email = document.getElementById("email").value;
+  var emailReg = /^.+@[^\.].*\.[a-z]{2,}$/;
+  if (!email.match(emailReg)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function displayMessage() {
+  let emailForm = document.getElementById("emailFormContainer");
+  let formMessage = document.getElementById("formMessage");
+
+  emailForm.className += " hidden";
+  formMessage.className += "active";
+}
+
+document.getElementById("emailForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  var xhr = new XMLHttpRequest();
+  var url = "https://api.hsforms.com/submissions/v3/integration/submit";
+	var portalId = "7637559";
+	var formId = "c6101fcd-c2e2-43a2-9c00-4b47cece6e10";
+  var finalUrl = url + "/" + portalId + "/" + formId;
+
+  let emailValue = document.getElementById("email").value;
+
+  let errorMessage = document.getElementById("errorMessage");
+
+  if (validation()) {
+    errorMessage.className = "";
+
+    let data = {
+      fields: [
+        {
+          name: "email",
+          value: emailValue,
+        },
+      ],
+      context: {
+        pageUri: "www.getistio.io",
+        pageName: "GetIstio Homepage"
+      },
+    };
+
+    fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((_) => {
+      displayMessage();
+    });
+  } else {
+    let formInput = document.getElementById("email");
+    formInput.className += "error";
+    errorMessage.className += "active";
+  }
+});
